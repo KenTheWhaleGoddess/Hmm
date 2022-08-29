9976dd6d2d33d33581b79360c0c0427cfa0c08f8
Ken Preiser <kenpreiser@gmail.com>
2022-08-29 01:01:26 -0700

    Create INLL.sol

---
+++ INLL.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.10;
+
+interface INLL {
+    function init(address _cloner, address _nft, address multisig) external;
+    function setNewCloner(address _cloner) external;
+    function setMultisig(address _multisig) external;
+    function pause() external;
+    function unpause() external;
+}
