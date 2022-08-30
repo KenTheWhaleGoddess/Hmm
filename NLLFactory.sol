da07ff4c72e487d78fc4581a626e0287a2b2c477
Ken Preiser <kenpreiser@gmail.com>
2022-08-30 09:00:18 -0700

    Update NLLFactory.sol

--- NLLFactory.sol
+++ NLLFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.8.10;
 
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,7 +10,7 @@ import "./INLL.sol";
 contract NLLFactory is Ownable {
 
     address currentImplementation;
-    uint256 public counter;
+    uint256 counter;
 
     mapping(address => uint256) nftToNllIndex;
     mapping(uint256 => address) deployedImplementations;
@@ -19,17 +19,10 @@ contract NLLFactory is Ownable {
     constructor (address _multisig, address _nft) {
         transferOwnership(_multisig);
         clone(_nft);
-        INLL(nftToDeployedImplementation(_nft)).pause();
+        INLL(getDeployedImplementationfromNFT(_nft)).pause();
     }
 
-    function nftToDeployedImplementation(address _nft) public view returns (address) {
-        uint c = nftToNllIndex[_nft];
-        //if (c == 0) return address(0); omitted for gas
-        return deployedImplementations[c];
-    }
-    function nftToNotLarvaLabsIndex(address _nft) public view returns (uint) {
-        return nftToNllIndex[_nft];
-    }
+    //clone
 
     function clone(address _nft) public returns (uint256) {
         require(nftToNllIndex[_nft] == 0, "we already have a NLL for this collection");
@@ -45,6 +38,34 @@ contract NLLFactory is Ownable {
         return counter;
     }  
 
+
+    //reading/frontend facing methods
+
+    function getCount() external view returns (uint256) {
+        return counter;
+    }
+    function getCurrentImplementation() external view returns (address) {
+        return currentImplementation;
+    }
+
+    function getDeployedImplementationfromNFT(address _nft) public view returns (address) {
+        uint c = nftToNllIndex[_nft];
+        //if (c == 0) return address(0); omitted for gas
+        return deployedImplementations[c];
+    }
+
+    function getDeployedImplementationfromIdx(uint256 idx) public view returns (address) {
+        //if (c == 0) return address(0); omitted for gas
+        return deployedImplementations[idx];
+    }
+    function nftToNotLarvaLabsIndex(address _nft) public view returns (uint) {
+        return nftToNllIndex[_nft];
+    }
+
+
+    //owner functions :o
+
+
     function migrateCloner(address newCloner) external onlyOwner {
         for(uint i; i < counter; i++) {
             INLL(deployedImplementations[i]).setNewCloner(newCloner);
@@ -52,24 +73,24 @@ contract NLLFactory is Ownable {
     }
 
     function pauseFor(address _address) external onlyOwner {
-        address nll = nftToDeployedImplementation(_address);
+        address nll = getDeployedImplementationfromNFT(_address);
         require(nll != address(0), "stop");
         INLL(nll).pause();
     }
     function unpauseFor(address _address) external onlyOwner {
-        address nll = nftToDeployedImplementation(_address);
+        address nll = getDeployedImplementationfromNFT(_address);
         require(nll != address(0), "stop");
         INLL(nll).unpause();
     }
     function pauseForIdx(uint c) external onlyOwner {
         require(c != 0, "stop");
-        address nll = deployedImplementations[c];
+        address nll = getDeployedImplementationfromIdx(c);
         require(nll != address(0), "drop");
         INLL(nll).pause();
     }
     function unpauseForIdx(uint c) external onlyOwner {
         require(c != 0, "stop");
-        address nll = deployedImplementations[c];
+        address nll = getDeployedImplementationfromIdx(c);
         require(nll != address(0), "drop");
         INLL(nll).unpause();
     }
@@ -85,7 +106,7 @@ contract NLLFactory is Ownable {
         }
     }
 
-    function setImplementation(address impl) external onlyOwner {
+    function setCurrentImplementation(address impl) external onlyOwner {
         currentImplementation = impl;
     }
 }
